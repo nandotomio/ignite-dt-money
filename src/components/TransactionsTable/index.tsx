064dd1d1fd4915784d432d36415fc5cb7a3919e0
Fernando Tomio <nandotomio@gmail.com>
2021-05-23 21:15:02 -0300

    feat: add TransactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website development</td>
+            <td className="deposit">U$ 12,000.00</td>
+            <td>Development</td>
+            <td>02/20/2021</td>
+          </tr>
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">- U$ 1,000.00</td>
+            <td>Home</td>
+            <td>02/25/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
